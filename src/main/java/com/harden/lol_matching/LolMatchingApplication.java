4983c34b1a36ff1ea067b1a64a4136f634d26617
hI <jktmobile6637@gmail.com>
2021-05-09 17:22:03 +0900

    Initial commit

---
+++ src/main/java/com/harden/lol_matching/LolMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.harden.lol_matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolMatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolMatchingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harden/lol_matching/LolMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harden.lol_matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolMatchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
