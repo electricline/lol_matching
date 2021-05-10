92a5380a105b3e5962873dd1fa2de4a3fa28a331
hI <jktmobile6637@gmail.com>
2021-05-11 00:16:00 +0900

    [REVISE] 연관관계 맵핑 + 빌더 적용

--- src/main/java/com/harden/lol_matching/entity/MatchingHistory.java
+++ src/main/java/com/harden/lol_matching/entity/MatchingHistory.java
@@ -1,8 +1,17 @@
 package com.harden.lol_matching.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
 public class MatchingHistory {
 
     @Id
@@ -10,5 +19,15 @@ public class MatchingHistory {
 
     @Column
     private String matchedId;
+
+    @OneToMany(mappedBy = "MatchingHistory")
+    private List<UserToMatchingHistory> userToMatchingHistories = new ArrayList<>();
+
+
+    @Builder
+    public MatchingHistory(String id, String matchedId){
+        this.id = id;
+        this.matchedId = matchedId;
+    }
 
 }
