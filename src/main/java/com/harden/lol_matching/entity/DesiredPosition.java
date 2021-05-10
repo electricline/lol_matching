42279f0a09baaafbfe825c0f492b69a88b7cbcdf
hI <jktmobile6637@gmail.com>
2021-05-11 00:15:41 +0900

    Auto stash before merge of "master" and "origin/master"
    [REVISE] 연관관계 맵핑 추가 + 빌더 패턴

--- src/main/java/com/harden/lol_matching/entity/DesiredPosition.java
+++ src/main/java/com/harden/lol_matching/entity/DesiredPosition.java
@@ -1,14 +1,32 @@
 package com.harden.lol_matching.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
 public class DesiredPosition {
 
     @Id
     private String id;
 
     @Column
-    private String Postion;
+    private String postion;
+
+    @OneToMany(mappedBy = "DesiredPosition")
+    private List<UserToDesiredPosition> userToDesiredPositions = new ArrayList<>();
+
+    @Builder
+    public DesiredPosition(String id, String position){
+        this.id = id;
+        this.postion = position;
+    }
 
 }
